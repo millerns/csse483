1000ca9a79a3b6de2b517a4f6949dc959e2cdee3
millerns <millerns@rose-hulman.edu>
2014-12-18 16:37:29 -0500

    11, Jersey, Lab 3 finished

--- 11-JerseyNSM/src/edu/rosehulman/millerns/jersey/JerseyInfo.java
+++ 11-JerseyNSM/src/edu/rosehulman/millerns/jersey/JerseyInfo.java
@@ -1,16 +1,49 @@
 package edu.rosehulman.millerns.jersey;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class JerseyInfo extends Activity {
+public class JerseyInfo extends Activity implements OnClickListener {
+
+	private Button mCancelButton;
+	private Button mOkButton;
+	private EditText mNameEdit;
+	private EditText mNumberEdit;
+	private ToggleButton mColorToggle;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_jersey_info);
+
+		Intent intent = getIntent();
+		String name = intent.getStringExtra(MainActivity.JERSEY_NAME_KEY);
+		int number = intent.getIntExtra(MainActivity.JERSEY_NUMBER_KEY,
+				MainActivity.DEFAULT_JERSEY_NUMBER);
+		boolean jerseyIsRed = intent.getBooleanExtra(
+				MainActivity.JERSEY_COLOR_KEY,
+				MainActivity.DEFAULT_JERSEY_COLOR);
+
+		mNameEdit = (EditText) findViewById(R.id.name_edit);
+		mNameEdit.setText(name);
+		mNumberEdit = (EditText) findViewById(R.id.number_edit);
+		mNumberEdit.setText("" + number);
+		mColorToggle = (ToggleButton) findViewById(R.id.jersey_color_toggle);
+		mColorToggle.setChecked(jerseyIsRed);
+
+		mCancelButton = (Button) findViewById(R.id.cancel_button);
+		mCancelButton.setOnClickListener(this);
+		mOkButton = (Button) findViewById(R.id.ok_button);
+		mOkButton.setOnClickListener(this);
 	}
 
 	@Override
@@ -31,4 +64,33 @@ public class JerseyInfo extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == mCancelButton.getId()) {
+			Intent returnIntent = new Intent();
+			setResult(RESULT_CANCELED, returnIntent);
+			this.finish();
+		} else if (v.getId() == mOkButton.getId()) {
+			int number = MainActivity.DEFAULT_JERSEY_NUMBER;
+			try {
+				number = Integer.parseInt(mNumberEdit.getText().toString());
+			} catch (NumberFormatException e) {
+				Toast.makeText(this,
+						"Number field must contain only an integer.",
+						Toast.LENGTH_SHORT).show();
+				return;
+			}
+			String name = mNameEdit.getText().toString();
+			boolean jerseyIsRed = mColorToggle.isChecked();
+			Intent returnIntent = new Intent();
+			returnIntent.putExtra(MainActivity.JERSEY_NAME_KEY, name);
+			returnIntent.putExtra(MainActivity.JERSEY_NUMBER_KEY, number);
+			returnIntent.putExtra(MainActivity.JERSEY_COLOR_KEY, jerseyIsRed);
+			setResult(RESULT_OK, returnIntent);
+			this.finish();
+		}
+
+	}
+
 }
--- 11-JerseyNSM/src/edu/rosehulman/millerns/jersey/MainActivity.java
+++ 11-JerseyNSM/src/edu/rosehulman/millerns/jersey/MainActivity.java
@@ -8,18 +8,29 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
 
 	private String mJerseyName;
 	private int mJerseyNumber;
+	private boolean mJerseyIsRed;
 
 	private TextView mJerseyNameView;
 	private TextView mJerseyNumberView;
+	private ImageView mJerseyImageView;
 
 	public static final String DEFAULT_JERSEY_NAME = "FISHER";
 	public static final int DEFAULT_JERSEY_NUMBER = 42;
+	public static final boolean DEFAULT_JERSEY_COLOR = true;
+
+	public static final String JERSEY_NAME_KEY = "JERSEY_NAME_KEY";
+	public static final String JERSEY_NUMBER_KEY = "JERSEY_NUMBER_KEY";
+	public static final String JERSEY_COLOR_KEY = "JERSEY_COLOR_KEY";
+
+	public static final int REQUEST_CODE_EDIT_JERSEY = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +42,11 @@ public class MainActivity extends Activity implements OnClickListener {
 
 		mJerseyNameView = (TextView) findViewById(R.id.jersey_name);
 		mJerseyNumberView = (TextView) findViewById(R.id.jersey_number);
+		mJerseyImageView = (ImageView) findViewById(R.id.jersey);
 
 		mJerseyName = DEFAULT_JERSEY_NAME;
 		mJerseyNumber = DEFAULT_JERSEY_NUMBER;
+		mJerseyIsRed = DEFAULT_JERSEY_COLOR;
 
 		updateJersey();
 	}
@@ -60,13 +73,37 @@ public class MainActivity extends Activity implements OnClickListener {
 	@Override
 	public void onClick(View v) {
 		Intent editIntent = new Intent(this, JerseyInfo.class);
-		startActivity(editIntent);
+		editIntent.putExtra(JERSEY_NAME_KEY, mJerseyName);
+		editIntent.putExtra(JERSEY_NUMBER_KEY, mJerseyNumber);
+		editIntent.putExtra(JERSEY_COLOR_KEY, mJerseyIsRed);
+		startActivityForResult(editIntent, REQUEST_CODE_EDIT_JERSEY);
 	}
 
 	private void updateJersey() {
 		mJerseyNameView.setText(mJerseyName);
 		mJerseyNumberView.setText("" + mJerseyNumber);
-		// mJerseyNumberView.setText(mJerseyNumber);
+		if (mJerseyIsRed) {
+			mJerseyImageView.setImageResource(R.drawable.red_jersey);
+		} else {
+			mJerseyImageView.setImageResource(R.drawable.blue_jersey);
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_CODE_EDIT_JERSEY) {
+			if (resultCode == RESULT_CANCELED) {
+				Toast.makeText(this, "Edit cancelled.", Toast.LENGTH_SHORT)
+						.show();
+			} else if (resultCode == RESULT_OK) {
+				mJerseyName = data.getStringExtra(JERSEY_NAME_KEY);
+				mJerseyNumber = data.getIntExtra(JERSEY_NUMBER_KEY,
+						DEFAULT_JERSEY_NUMBER);
+				mJerseyIsRed = data.getBooleanExtra(JERSEY_COLOR_KEY,
+						DEFAULT_JERSEY_COLOR);
+				updateJersey();
+			}
+		}
 	}
 
 }
